6d52d371a604838f170af5a1c8d82757c2aab600
imbillah <imbilla99@gmail.com>
2023-04-30 21:52:54 -0400

    responsive navbar implemented

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -12,6 +12,7 @@ import {
   MenuDivider,
   MenuItem,
   MenuList,
+  Spacer,
   Text,
 } from "@chakra-ui/react";
 import { IoSearch } from "react-icons/io5";
@@ -19,36 +20,38 @@ import { RiVideoUploadLine } from "react-icons/ri";
 
 const Navbar = ({ user }) => {
   return (
-    <Box bg={"black"} color={"white"}>
+    <Box>
       <Flex
         justifyContent={"space-between"}
         alignItems={"center"}
-        width={"100vw"}
-        p={4}
+        py={4}
+        px={["10px", "30px"]}
+        bg={"black"}
+        color={"white"}
       >
-        {/* Logo item */}
-        <Box>
-          <Link to={"/"}>
-            <Flex alignItems={"center"}>
-              <Image src={Logo} width={"50px"} height={"50px"} />
-
-              <Text
-                fontSize={"4xl"}
-                fontWeight={"bold"}
-                color={"#FF0000"}
-                marginLeft={2}
-              >
-                Motion
-              </Text>
-            </Flex>
-          </Link>
-        </Box>
-        {/* serach box */}
+        {/* logo */}
+        <Flex alignItems={"center"}>
+          <Box>
+            <Link to={"/"}>
+              <Image src={Logo} width={45} height={45} />
+            </Link>
+          </Box>
+          <Text
+            marginStart={2}
+            fontSize={"3xl"}
+            color={"red"}
+            fontWeight={"bold"}
+            hideBelow="md"
+          >
+            Motion
+          </Text>
+        </Flex>
+        {/* search box */}
         <Box border="2px" p={2} borderRadius="full" borderColor="gray.200">
           <Flex alignItems={"center"}>
             <IoSearch size={25} />
             <Input
-              width={"60vw"}
+              width={{ md: "50vw" }}
               variant="unstyled"
               _focus={{ outline: "none" }}
               placeholder="Search"
@@ -58,37 +61,39 @@ const Navbar = ({ user }) => {
             />
           </Flex>
         </Box>
-        {/* userinfo */}
-        <Flex justifyContent={"center"} alignItems={"center"}>
-          <Box marginRight={4}>
-            <Link to={"/upload"}>
-              <RiVideoUploadLine size={30} color="red" />
-            </Link>
-          </Box>
-          <Box
-            bg={"white"}
-            height={"40px"}
-            width={"40px"}
-            borderRadius={"full"}
-            textColor={"black"}
-          >
-            <Menu>
-              <MenuButton>
-                <Image
-                  src={user?.photoURL}
-                  width={"40px"}
-                  height={"40px"}
-                  rounded={"full"}
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Download</MenuItem>
-                <MenuDivider />
-                <MenuItem>Log Out</MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
-        </Flex>
+        {/* profile */}
+        <Box>
+          <Flex alignItems={"center"}>
+            <Box marginRight={4}>
+              <Link to={"/upload"}>
+                <RiVideoUploadLine size={30} />
+              </Link>
+            </Box>
+            <Box
+              bg={"white"}
+              height={"40px"}
+              width={"40px"}
+              borderRadius={"full"}
+              textColor={"black"}
+            >
+              <Menu>
+                <MenuButton>
+                  <Image
+                    src={user?.photoURL}
+                    width={"40px"}
+                    height={"40px"}
+                    rounded={"full"}
+                  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Download</MenuItem>
+                  <MenuDivider />
+                  <MenuItem>Log Out</MenuItem>
+                </MenuList>
+              </Menu>
+            </Box>
+          </Flex>
+        </Box>
       </Flex>
     </Box>
   );
